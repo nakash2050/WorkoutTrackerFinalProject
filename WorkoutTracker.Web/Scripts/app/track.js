31634633736e7a4768737f156a74377d6c6db7ae
Nakash Sheikh <nakash2050@gmail.com>
2018-04-20 14:56:34 +0530

    Completed "Track"  View

--- WorkoutTracker.Web/Scripts/app/track.js
+++ WorkoutTracker.Web/Scripts/app/track.js
@@ -1,20 +1,34 @@
 ﻿$(function () {
     var route = './Track/GetTrackReports';
     $.get(route, function (resp) {
-        console.log(resp);
+       
         $('#totalWorkoutTimeOfDay').html(Math.abs(resp.TotalWorkoutTimeOfDay) + ' mins');
         $('#totalWorkoutTimeOfWeek').html(Math.abs(resp.TotalWorkoutTimeOfMonth) + ' mins');
         $('#totalWorkoutTimeOfMonth').html(Math.abs(resp.TotalWorkoutTimeOfWeek) + ' mins');
 
-        var ctx = document.getElementById("myChart").getContext('2d');
-        var myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange", "Sunday", "Monday", "Trim", "Trim2", "TRIM", "TREE"],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3, 4, 5, 10, 5, 6, 9],
-                    backgroundColor: [
+        var weekDataLabels = [];
+        var weekData = [];
+        var monthData = [];
+        var monthDataLabels = [];
+        var yearData = [];
+        var yearDataLabels = [];
+
+        $.each(resp.TotalCaloriesBurntPerWeek, function (i, v) {
+            weekDataLabels.push(v.Duration);
+            weekData.push(Math.abs(v.TotalCaloriesBurnt));
+        });
+
+        $.each(resp.TotalCaloriesBurntPerMonth, function (i, v) {
+            monthDataLabels.push(v.Duration);
+            monthData.push(Math.abs(v.TotalCaloriesBurnt));
+        });
+
+        $.each(resp.TotalCaloriesBurntPerYear, function (i, v) {
+            yearDataLabels.push(v.Duration);
+            yearData.push(Math.abs(v.TotalCaloriesBurnt));
+        });
+
+        var backgroundColors = [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
                         'rgba(255, 206, 86, 0.2)',
@@ -28,8 +42,9 @@
                         'rgba(153, 7, 206, 0.2)',
                         'rgb(0, 136, 204, 0.2)'
 
-                    ],
-                    borderColor: [
+        ];
+
+        var borderColors = [
                         'rgba(255,99,132, 1)',
                         'rgba(54, 162, 235, 1)',
                         'rgba(255, 206, 86, 1)',
@@ -42,7 +57,18 @@
                         'rgba(255, 34, 51, 1)',
                         'rgba(153, 7, 206, 1)',
                         'rgb(0, 136, 204, 1)'
-                    ],
+        ];
+
+        var ctx = document.getElementById("chartWeekData").getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: weekDataLabels,
+                datasets: [{
+                    label: 'Calories Burnt Per Week',
+                    data: weekData,
+                    backgroundColor: backgroundColors,
+                    borderColor: borderColors,
                     borderWidth: 1
                 }]
             },
@@ -56,5 +82,54 @@
                 }
             }
         });
+
+        var ctx = document.getElementById("chartMonthData").getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: monthDataLabels,
+                datasets: [{
+                    label: 'Calories Burnt Per Month',
+                    data: monthData,
+                    backgroundColor: backgroundColors,
+                    borderColor: borderColors,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+
+        var ctx = document.getElementById("chartYearData").getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: yearDataLabels,
+                datasets: [{
+                    label: 'Calories Burnt Per Year',
+                    data: yearData,
+                    backgroundColor: backgroundColors,
+                    borderColor: borderColors,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+
     });
 })
